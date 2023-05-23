0b43bfcc55a2f9568593ff082c57ebe4779df60e
Pachitokun <125917683+Pachitokun@users.noreply.github.com>
2023-05-23 10:34:07 -0500

    Add files via upload

---
+++ js/moskepon.js
@@ -0,0 +1,343 @@
+const botonmascotajugador = document.getElementById('botonmascota')
+const botonfuego = document.getElementById('botonagua')
+const botonagua = document.getElementById('botonfuego')
+const botontierra = document.getElementById('botontierra')
+const ButtomReload= document.getElementById('botonreinicar')
+
+
+const spanmascotajugador = document.getElementById('mascotajugador')
+
+const spanvidasjugador = document.getElementById("vidasjugador")
+const spanvidasenemigo = document.getElementById("vidasenemigo")
+
+const SectionMensajes = document.getElementById("mensajes")
+const contenedorTarjetas = document.getElementById("contenedorTarjetas")
+
+const sectionVerMapa = document.getElementById("seemap")
+const mapa = document.getElementById("map")
+
+let mokepones = []
+let ataquejugador
+let ataqueenemigo
+let OpcionDeMokepones
+let inputgreninja
+let inputcharizad 
+let inputvulvasur 
+let vidasjugador = 3
+let vidasenemigo = 3
+let mascotaJugador
+let lienzo = map.getContext("2d")
+let intervalo
+let mapBackground = new Image()
+mapBackground.src = "./Images/map.jpg"
+
+class Mokepon 
+{
+    constructor(Name, Photo, Life) 
+    {
+        this.Name = Name
+        this.Photo = Photo
+        this.Life = Life
+        this.attack = []
+        this.x = 20
+        this.y = 30
+        this.ancho = 80
+        this.alto = 80
+        this.mapPhoto = new Image()
+        this.mapPhoto.src = Photo
+        this.speedX = 0
+        this.speedY = 0
+    }
+}
+
+let greninja = new Mokepon("Greninja", "./Images/greninja_mokepon.png", 5)
+let charizad = new Mokepon("Charizad", "./Images/Charizard_mokepon.png", 5)
+let vulvasur = new Mokepon("Vulvasur", "./Images/Bulbasour_mokepon.png", 5)
+
+
+greninja.attack.push(
+    { Name: "💧", id: "botonagua" },
+    { Name: "💧", id: "botonagua" },
+    { Name: "💧", id: "botonagua" },
+    { Name: "🔥", id: "botonfuego" },
+    { Name: "🌱", id: "botontierra" },
+)
+charizad.attack.push(
+    { Name: "🔥", id: "botonagua" },
+    { Name: "🔥", id: "botonagua" },
+    { Name: "🔥", id: "botonagua" },
+    { Name: "🌱", id: "botonfuego" },
+    { Name: "💧", id: "botontierra" },
+)
+vulvasur.attack.push(
+    { Name: "🌱", id: "botonagua" },
+    { Name: "🌱", id: "botonagua" },
+    { Name: "🌱", id: "botonagua" },
+    { Name: "💧", id: "botonfuego" },
+    { Name: "🔥", id: "botontierra" },
+)
+
+mokepones.push(greninja, charizad, vulvasur)
+
+function iniciarjuego() {
+
+    sectionVerMapa.style.display = "none"
+
+    mokepones.forEach((Mokepon) => {
+        OpcionDeMokepones = ` 
+        <input type="radio" name="mascota" id=${Mokepon.Name} />
+        <label class="tarjeta-de-mokepon" for=${Mokepon.Name} ></label>
+        <div class="card" >
+                    <div class="card-image"><img src=${Mokepon.Photo} alt=${Mokepon.Name}
+                    width="260px" height="240px"></div>
+                <div class="card-text">
+                    <span class="date-1">1 days ago</span>
+                    <h2><i class="fa-fire"></i> ${Mokepon.Name} </h2>
+                    <p>es un Pokémon de tipo agua/siniestro introducido en la sexta generación. 
+                        Es la evolución de Frogadier.</p>
+                </div>
+                <div class="card-stats card-1">
+                    <div class="stat">
+                        <div class="value">40,0 kg</div>
+                        <div class="type">agua/siniestro</div>
+                    </div>
+                    <div class="border"></div>
+                    <div class="stat">
+                        <div class="value">5123</div>
+                        <div class="type">views</div>
+                    </div>
+                    <div class="border"></div>
+                    <div class="stat">
+                        <div class="value">32</div>
+                        <div class="type">comment</div>
+                    </div>
+        </div>`
+        contenedorTarjetas.innerHTML += OpcionDeMokepones
+
+        inputgreninja = document.getElementById("Greninja")
+        inputgreninja = document.getElementById("Charizad")
+        inputgreninja = document.getElementById("Vulvasur")
+    })
+    botonmascotajugador.addEventListener("click", seleccionarmascotajugador)
+    botonfuego.addEventListener("click", ataquefuego)
+    botonagua.addEventListener("click", ataqueagua)
+    botontierra.addEventListener("click", ataquetierra)
+    
+   
+}
+
+function seleccionarmascotajugador() {
+    
+    sectionVerMapa.style.display = "flex"
+    startMap()
+    
+   
+    lienzo.drawImage(
+        imagenDeGreninja,
+        20,
+        40,
+        100,
+        100
+    )
+        
+
+    if (inputgreninja.checked) {
+        spanmascotajugador.innerHTML = inputgreninja.id
+        mascotaJugador = inputgreninja.id
+    }else if (inputcharizad.checked) {
+        spanmascotajugador.innerHTML = inputcharizad.id
+        mascotaJugador = inputgreninja.i
+    } else if (inputvulvasur.checked) {
+        spanmascotajugador.innerHTML = inputvulvasur.id
+        mascotaJugador = inputvulvasur.id
+    } else {
+        alert('selecciona una mascota')
+    }
+    
+}
+function extraerAtaques(mascotaJugador) {
+    let Attacks
+    for(let i = 0; i < mokepones.length; i++) {
+        if (mascotaJugador === mokepones[i].Name) {
+            Attacks = mokepones[i].Attacks
+        }
+    }
+}
+
+    function seleccionarmascotaenemigo() {
+    let mascotaaleatorio = aleatorio(1,3)
+    let spanmascotaenemigo = document.getElementById(mascotaenemigo)
+    
+    if(mascotaaleatorio == 1) {
+        spanmascotaenemigo.innerHTML = 'greninja'
+    } else if (mascotaaleatorio == 2) {
+        spanmascotaenemigo.innerHTML = 'charizad'
+    } else {
+        spanmascotaenemigo.innerHTML = 'vulvasur'
+    }
+}
+
+function ataquefuego() {
+    ataquejugador = "fuego"
+    ataquealeatorioenemigo()
+}
+function ataqueagua() {
+    ataquejugador = "agua"
+    ataquealeatorioenemigo()
+}
+function ataquetierra() {
+    ataquejugador = "tierra"
+    ataquealeatorioenemigo()
+}
+
+function ataquealeatorioenemigo() {
+    let ataquealeatorio = aleatorio(1,3)
+
+    if (ataquealeatorio == 1 ) {
+        ataqueenemigo = 'fuego'
+    } else if (ataquealeatorio == 2) {
+        ataqueenemigo = 'agua'
+    } else {
+        ataqueenemigo = 'tierra'
+    }
+
+
+    crearmensajes()
+}
+
+function combate() {
+
+    if(ataqueenemigo  == ataquejugador) {
+        crearmensajes("empate")
+    } else if (ataquejugador == 'fuego' && ataqueenemigo == 'tierra') {
+        crearmensajes("ganaste")
+        vidasenemigo--
+        spanvidasenemigo.innerHTML = 'vidasenemigo'
+    } else if (ataquejugador == agua && ataqueenemigo == 'fuego') {
+        crearmensajes("ganaste")
+        vidasenemigo--
+        spanvidasenemigo.innerHTML = 'vidasenemigo'
+        
+    } else if (jugador == tierra && ataqueenemigo == 'agua') {
+        crearmensajes("ganaste")
+        vidasenemigo--
+        spanvidasenemigo.innerHTML = 'vidasenemigo'
+    } else {
+        crearmensajes("perdiste")
+        vidasjugador--
+        spanvidasjugador.innerHTML = 'vidasjugador'
+    }
+}
+
+function revisarvidas() {
+    if (vidasenemigo == 0){
+        crearmensajesfinal("felicitaciones ganaste")
+    } else if (vidasjugador == 0) {
+        crearmensajesfinal("lo siento perdiste")
+    }
+}
+
+function crearmensajes(resultado) {
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'tu mascota ataco con' + ataquejugador + ',la mascota del enemigo ataco con ' + ataqueenemigo  + '- ' + resultado 
+
+    sectionmensajes.appendChild(parrafo)
+}
+
+function crearmensajesfinal(resultadofinal) {
+    
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'tu mascota ataco con' + ataquejugador + ',la mascota del enemigo ataco con ' + ataqueenemigo   + '- ' + resultado
+
+    sectionmensajes.appendChild(parrafo)
+}
+
+function reiniciarjuego() {
+    location.reload()
+}
+
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
+    
+function painCanvas() {
+
+   greninja.x = greninja.x + greninja.speedX
+   greninja.y = greninja.y + greninja.speedY
+    lienzo.clearRect(0, 0, map.width, map.height)
+    lienzo.drawImage(
+        mapBackground,
+        0,
+        0,
+        map.width,
+        map.height
+        
+        
+    )
+    lienzo.drawImage(
+    greninja.mapPhoto,
+    greninja.x,
+    greninja.y,
+    greninja.ancho,
+    greninja.alto
+   ) 
+}
+
+function MovingLeft() 
+{
+    greninja.speedX = -5
+}
+function MovingRight() 
+{
+    greninja.speedX = 5
+}
+function MovingDown() 
+{
+    greninja.speedY = 5
+}
+function MovingTop() 
+{
+    greninja.speedY = -5
+}
+function StopMoving() 
+{
+    greninja.speedX = 0
+    greninja.speedY = 0
+}
+
+function sePrecinoUnaTecla(event) {
+    switch (event.key) {
+        case "ArrowUp":
+            MovingTop()
+            break
+        case "ArrowDown":
+            MovingDown()
+            break
+        case "ArrowLeft":
+            MovingLeft()
+            break
+        case "ArrowRight":
+            MovingRight()
+            break
+    
+        default:
+            break
+    }
+}
+
+function startMap() {
+    map.width = 1000
+    map.height = 550
+    intervalo = setInterval(painCanvas, 50)
+    window.addEventListener("keydown", sePrecinoUnaTecla)
+    window.addEventListener("keyup", StopMoving)
+}
+
+
+
+window.addEventListener("load", iniciarjuego)
